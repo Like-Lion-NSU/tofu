760f4f30f045fa5cfe0eb354d790cb949ffab2c5
조아빈 <whdkqls122@naver.com>
2023-10-02 22:57:00 +0900

    테스트 페이지 완 (질문  제외)

--- FE/tofu_fe/src/App.js
+++ FE/tofu_fe/src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Mainpage from './pages/Mainpage'
 import Testpage from './pages/Testpage'
+import Resultpage from './pages/Resultpage'
+import './App.css'
 
 export default function App(){  // Main 화면
   return(
@@ -9,6 +11,7 @@ export default function App(){  // Main 화면
       <Routes>
         <Route path='/' element={<Mainpage/>}/>
         <Route path='/testpage' element={<Testpage/>}/>
+        <Route path='/resultpage' element={<Resultpage/>}/>
       </Routes>
     </BrowserRouter>
   )
--- FE/tofu_fe/src/pages/Testpage.js
+++ FE/tofu_fe/src/pages/Testpage.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, {useState} from "react";
+import '../css/Testpage.css'
+import { useNavigate } from "react-router";
+import axios from 'axios'
 
 export default function Testpage(){
+    const [Index, setIndex] = useState(0)
+    const [selected, setSelected] = useState([])  //선택한 답변 리스트
+    const navigate = useNavigate()
+
+    const SelectA = ['E', 'N', 'F', 'P', 'I', 'S', 'T', 'J', 'E', 'N', 'F', 'P']
+    const SelectB = ['I', 'S', 'T', 'J', 'E', 'N', 'F', 'P', 'I', 'S', 'T', 'J']
+    const QuestionA = ['E', 'N', 'F', 'P', 'I', 'S', 'T', 'J', 'E', 'N', 'F', 'P']
+    const QuestionB = ['I', 'S', 'T', 'J', 'E', 'N', 'F', 'P', 'I', 'S', 'T', 'J']
+
+    const PostSelctList = async() => {
+        try{
+            const response = await axios.post('/api/mbti', selected)
+            const mbti = response.data.mbti
+            navigate('/resultpage', {state:{mbti:mbti}})
+        } catch(err){
+            console.log(err)
+        }
+    }
+
+    const UserSelect = e =>{
+        console.log(e.target.title)
+        setSelected([...selected, e.target.title])
+        if(Index === 11){
+            selected.push(e.target.title)
+            PostSelctList()
+        }else{
+            setIndex(Index => Index+1)
+        }
+    }
+    
+    const progressBarWidth = ((Index + 1) / QuestionA.length) * 100 + "%";
+    const progressBarStyle = { width: progressBarWidth };
+
     return(
-        <>
-            hi
-        </>
+        <div className="testpagebox">
+            <div className="progress-bar-container">
+                <div className="progress-bar" style={progressBarStyle}></div>
+                <div className="progress-number">{Index + 1}/{QuestionA.length}</div>
+            </div>
+            <div className="Testtext">상황</div>
+            <div className="selectbox">
+                <div className="selectbtn selecta" title={SelectA[Index]} onClick={UserSelect}>{QuestionA[Index]}</div>
+                <div style={{fontSize:'2rem'}}>vs</div>
+                <div className="selectbtn selectb" title={SelectB[Index]} onClick={UserSelect}>{QuestionB[Index]}</div>
+            </div>
+        </div>
     )
 }
